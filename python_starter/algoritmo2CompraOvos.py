f2b578e62fb4e2cb28c972fcdd29b8273e9e2e33
Matheus Bernardes <matheusbnas@gmail.com>
2024-06-18 15:00:06 -0300

    Correcao código

--- python_starter/algoritmo2CompraOvos.py
+++ python_starter/algoritmo2CompraOvos.py
@@ -10,21 +10,21 @@ def abrir_geladeira(qtd_ovos):
 
 #comprar ovos se a qtd de ovos for menor que 8.
 def fazer_compras(ovos):
-    print("----Pegar a carteira.....")
-    print("......fazendo compras......")
+    print(".....Pegar a carteira.....")
+    print("......Ir para o mercado fazer compras......")
     print('Enquanto isso vou comprando ovos no mercado.')
     print()
 
-    cartela_ovos = []
+    ovos_faltantes = []
     while ovos < 8:
+        ovos_faltantes.append(ovos)
         ovos += 1
         if ovos == 8:
-            qtd_ovos_comprados = 0
             print("......Indo para casa......")
-            print(f'Comprei os {ovos} ovos. Agora posso ir para casa.')
+            print(f'Comprei os {len(ovos_faltantes)} ovos que faltavam. Agora posso ir para casa.')
     
 
-    return fazer_bolo(cartela_ovos, tempo)
+    return fazer_bolo(len(ovos_faltantes), tempo)
 
 def fazer_bolo(caixa_ovos, tempo):
 
@@ -33,11 +33,11 @@ def fazer_bolo(caixa_ovos, tempo):
     else: 
         while tempo:
             tempo += 1
-            if tempo == 15:
+            if tempo <= 15:
                 print()
                 print(f'Tempo atual de cozimento: {tempo} minutos.')
                 print("......fazendo o bolo......")
-            elif tempo == 30:   
+            elif tempo <= 30:   
                 print()
                 print(f'Tempo atual de cozimento: {tempo} minutos.')
                 print("......fazendo o bolo......")
